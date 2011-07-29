b224830684651d0500b9476c18479b18d6545f8e
Pradeep B V <pradeepbv@gmail.com>
2011-07-29 20:01:01 +0530

    changing worker messaging

--- scripts/work.js
+++ scripts/work.js
@@ -1,13 +1,13 @@
 var end=1e8,tmp=1;
 
-postMessage('start');
+postMessage('start to count down from ' + end);
 
 while(end) {
 	
 	end -= 1;
 	tmp += end;
 	if (end === 5e7) {
-		postMessage('halfway there, tmp is now ' + tmp);
+		postMessage('halfway there, I am at ' + tmp + ' now');
 	}
 }
 
